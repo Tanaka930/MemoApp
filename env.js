a7f4fd5e179b1150238968c6b9b28cc3a0cb22d2
Yuma Tanaka <55372083+Tanaka930@users.noreply.github.com>
2022-07-11 15:52:04 +0900

    Delete env.js

--- env.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable-next-line */
-export const firebaseConfig = {
-  apiKey: 'AIzaSyCFEqY1biwG0so4c_puKxtsd6f5JOdC0bk',
-  authDomain: 'memoapp-5eded.firebaseapp.com',
-  projectId: 'memoapp-5eded',
-  storageBucket: 'memoapp-5eded.appspot.com',
-  messagingSenderId: '942222137437',
-  appId: '1:942222137437:web:821a428b4b4a30ccbad5b4',
-};
